f26018943bf8381092ba83859fd89fdc0dd5288d
Aravinth <aravinth.s@sonata-software.com>
2025-04-22 16:26:58 +1000

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -43,4 +44,7 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_array,test_array,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    ModelTrainer=ModelTrainer()
+    print(ModelTrainer.initiate_model_trainer(train_array,test_array))
